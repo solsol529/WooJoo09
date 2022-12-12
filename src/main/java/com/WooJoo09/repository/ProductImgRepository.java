a3df9025859c8401a02332b7d2b669f34710306e
EunjiShinRabbit <dmmswll97@gmail.com>
2022-12-12 21:28:31 +0900

    12/12 최종 커밋

---
+++ src/main/java/com/WooJoo09/repository/ProductImgRepository.java
@@ -0,0 +1,10 @@
+package com.WooJoo09.repository;
+
+import com.WooJoo09.entity.Trade;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductImg extends JpaRepository<ProductImg, Long> {
+    List<ProductImg> findByTradeNum(Trade tradeNum);
+}
